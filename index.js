66d096d97f3179717a3c36611d5ab3d2ff86b77d
TVHstudio <93575862+TVHstudio@users.noreply.github.com>
2022-11-05 07:43:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+
+let app = express();
+app.use(express.static(__dirname + '/public'));
+
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
+    extname : 'hbs',
+    defaultLayout : 'layouts',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir:__dirname + '/views/partials',
+    runtimeOptions : {allowProtoPropertiesByDefault : true},
+    // helpers: {
+    //     createStarList : helper.createStarList,
+    //     createStars : helper.createStars,
+
+    // }   
+});
+app.engine('hbs',hbs.engine);
+app.set('view engine', 'hbs');  
+
+app.get('/sync',(req,res) => {
+    let models = require('./models');
+    models.sequelize.sync()
+    .then(()=>{
+        res.send('database sync completed!')
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), ()=> {
+    console.log(`Server is running on port ${app.get('port')}`);
+})
